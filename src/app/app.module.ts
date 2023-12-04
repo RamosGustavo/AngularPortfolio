27962961bfe295eb8ff5ddf5dd3c075f8d94dff2
RamosGustavo <gr4030077@gmail.com>
2023-12-04 09:54:28 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from './profile/about/about.component';
 import { ContactComponent } from './profile/contact/contact.component';
 import { EducationComponent } from './profile/education/education.component';
 import { ExperiencieComponent } from './profile/experiencie/experiencie.component';
 import { FooterComponent } from './profile/footer/footer.component';
 import { HeaderComponent } from './profile/header/header.component';
-import { IntroComponent } from './profile/intro/intro.component';
 import { SkillsComponent } from './profile/skills/skills.component';
 
 @NgModule({
@@ -21,7 +20,6 @@ import { SkillsComponent } from './profile/skills/skills.component';
     ExperiencieComponent,
     FooterComponent,
     HeaderComponent,
-    IntroComponent,
     SkillsComponent
   ],
   imports: [
